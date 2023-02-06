98556b80aaf1cc6d4b69b533575fbb2f1cbc85ed
AllanBernier <allan.bernier1@gmail.com>
2023-02-06 16:31:37 +0100

    Characters MVC Create

--- Followers-MVC/src/main/java/DAO/UtilConnection.java
+++ Followers-MVC/src/main/java/DAO/UtilConnection.java
@@ -1,5 +1,32 @@
 package DAO;
 
-public class UtilConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class UtilConnection {
+	
+	public static Connection seConnecter() {
+		
+		String DRIVER = "com.mysql.cj.jdbc.Driver";
+		String URL= "jdbc:mysql://localhost:3306/hello";
+		String LOGIN= "root";
+		String PASSWORD= "root";
+		
+		Connection connection = null;
+		
+		try {
+			Class.forName(DRIVER);
+			connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+		} catch (SQLException e) {
+			System.out.println("SQLException");
+		} catch (ClassNotFoundException e1) {
+			System.out.println("ClassNotFoundException");
+		}
+		
+				
+		return connection;
+	}	
+	
+	
 }
--- Followers-MVC/src/main/webapp/CreateView.jsp
+++ Followers-MVC/src/main/webapp/CreateView.jsp
@@ -7,6 +7,23 @@
 <title>Create View</title>
 </head>
 <body>
-
+	<h1>Create</h1>
+	
+	<form action="/Followers-MVC/add" method="post">
+		<label for='txtName'>Nom</label>
+		<input type='text' name='txtName'> <br/>
+		
+		<label for='txtLevel'>Niveau</label>
+		<input type='number' name='txtLevel'> <br/>
+		
+		<label for='txtSize'>Taille</label>
+		<input type='number' name='txtSize'> <br/>
+		
+		<label for='txtType'>Type</label>
+		<input type='text' name='txtType'> <br/>
+		
+		<input type='submit' value='Valider'>
+	</form>
+	
 </body>
 </html>
